aff9c2b2951ab2e2561016c59e5e8ce7b507e956
AQB <abrown0926>
2021-12-01 18:49:27 -0600

    connected to express server and db

---
+++ Develop/server.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const dbConnection = require("./database/config");
+const app = express();
+const port = 3000;
+
+dbConnection();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
