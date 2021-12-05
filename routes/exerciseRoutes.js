2d643776ed5b30959d2e324824635afc0d484e3c
AQB <abrown0926>
2021-12-04 23:06:23 -0600

    updated routes

--- routes/exerciseRoutes.js
+++ routes/exerciseRoutes.js
@@ -10,15 +10,3 @@ router.get("/stats", (req, res) => {
 });
 
 module.exports = router;
-
-// const path = require("path");
-
-// module.exports = function (app) {
-//   app.get("/exercise", function (req, res) {
-//     res.sendFile(path.join(__dirname, "../public/exercise.html"));
-//   });
-
-//   app.get("/stats", function (req, res) {
-//     res.sendFile(path.join(__dirname, "../public/stats.html"));
-//   });
-// };
